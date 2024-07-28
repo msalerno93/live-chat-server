9bd2871145c97d369b0ee8a8604597b59bce808e
Michael Salerno <msalerno1993@gmail.com>
2024-07-28 12:04:01 -0400

    setting up db and ensuring register/login functionality

--- server.js
+++ server.js
@@ -8,55 +8,71 @@ const bcrypt = require("bcrypt");
 
 const app = express();
 const server = http.createServer(app);
-const port = 5000;
-const JWT_SECRET = "fakeJWTtoken";
 
 const io = socketIo(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
+cors: {
+origin: "",
+methods: ["GET", "POST"],
+},
 });
-
-const db = new sqlite3.Database(":memory:");
 
 app.use(cors());
 app.use(express.json());
 
+const PORT = 5000;
+const JWT_SECRET = "FAKEJWTTOKEN";
+
+const db = new sqlite3.Database("./chat-app.db");
+
 db.serialize(() => {
-  db.run(
-    "CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT)"
-  );
-  db.run(
-    "CREATE TABLE messages (id INTEGER PRIMARY KEY, senderId INTEGER, receiverId INTEGER, message TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)"
-  );
+db.run(
+"CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT)"
+);
+db.run(
+"CREATE TABLE messages (id INTEGER PRIMARY KEY, senderId INTEGER, receiverId INTEGER, message TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)"
+);
 });
 
 const authMiddleware = (req, res, next) => {
-    const token = req.headers.authorization
-    if(!token){
-        return res.status(401).send('Access Denied')
-    }
-    try {
-        const verified = jwt.verify(token, JWT_SECRET)
-        req.user = verified
-        next()
-    } catch (error) {
-        res.status(400).send('Invalid Token')
-    }
+const token = req.headers.authorization;
+if (!token) return res.status(401).send("Access Denied");
+try {
+const verified = jwt.verify(token, JWT_SECRET);
+req.user = verified;
+next();
+} catch (err) {
+res.status(400).send("Invalid Token");
 }
+};
 
-app.post('/api/register', (req, res) => {
-    const [username, password] = req.body
-    const hashedPassword = bcrypt.hashSync(password, 10)
-    db.run("INSERT INTO users (username, password) VALUES (?, ?)", [username, hashedPassword], (err) => {
-        if(err){
-            return res.status(400).send('Username already exists!')
-        }
-        res.send('User Registered Successfully!')
-    })
-})
+app.post("/api/register", (req, res) => {
+const { username, password } = req.body;
+const hashedPassword = bcrypt.hashSync(password, 10);
+db.run(
+"INSERT INTO users (username, password) VALUES (?, ?)",
+[username, hashedPassword],
+(err) => {
+if (err) {
+return res.status(400).send("Username already exists");
+}
+res.send("User Registered!");
+}
+);
+});
 
+app.post("/api/login", (req, res) => {
+const { username, password } = req.body;
+db.get("SELECT * FROM users WHERE username = ?", [username], (err, user) => {
+const validPass = bcrypt.compareSync(password, user.password);
+if (err || !user || !validPass) {
+return res.status(400).send("Invalid username or password");
+}
+const token = jwt.sign(
+{ id: user.id, username: user.username },
+JWT_SECRET
+);
+res.header("auth-token", token).send(token);
+});
+});
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
