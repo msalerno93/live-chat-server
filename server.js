79a96600b010d2f8982390b9eb0c885e1745ab6b
Michael Salerno <msalerno1993@gmail.com>
2024-07-28 12:13:47 -0400

    add .env for jwt protection

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const sqlite3 = require("sqlite3");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+require('dotenv').config()
+
 
 const app = express();
 const server = http.createServer(app);
@@ -19,8 +21,6 @@ methods: ["GET", "POST"],
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
-const JWT_SECRET = "FAKEJWTTOKEN";
 
 const db = new sqlite3.Database("./chat-app.db");
 
@@ -37,7 +37,7 @@ const authMiddleware = (req, res, next) => {
 const token = req.headers.authorization;
 if (!token) return res.status(401).send("Access Denied");
 try {
-const verified = jwt.verify(token, JWT_SECRET);
+const verified = jwt.verify(token, process.env.JWT_SECRET);
 req.user = verified;
 next();
 } catch (err) {
@@ -69,10 +69,10 @@ return res.status(400).send("Invalid username or password");
 }
 const token = jwt.sign(
 { id: user.id, username: user.username },
-JWT_SECRET
+process.env.JWT_SECRET
 );
 res.header("auth-token", token).send(token);
 });
 });
 
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+server.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
